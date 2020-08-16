a56393e14ddba6b64fce4109a9e84712601373c9
Yitzik Casapu <yitzikc@gmail.com>
2020-08-16 08:27:42 +0100

    Add a resful route for setting a single routing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { createServer, Request, Response, plugins } from 'restify';
 import { setRoutingOnNginx } from './routing-control';
 
-const setRouting = (req: Request, res: Response, next: any) => {
+const setRouting = (
+  routings: object, res: Response, next: any
+) => {
     // FIXME: Set the type of next to something sensible!
-    console.log("Got request %s", JSON.stringify(req.body));
-    setRoutingOnNginx(req.body).then( (result: any) => {
-      res.send({ mappings: req.body });
+    console.log("Applying routings %s", JSON.stringify(routings));
+    setRoutingOnNginx(routings).then( (result: any) => {
+      res.send({ mappings: routings });
     },
     (error: any) => {
       res.status(500);
@@ -20,7 +22,30 @@ server.use(plugins.bodyParser({
 }));
 
 
-server.post('/api/v1/routing', setRouting);
+const setMultipleRoutings = (
+  req: Request, res: Response, next: any
+) => {
+  const routings = req.body;
+  setRouting(routings, res, next);
+}
+
+server.post('/api/v1/routing', setMultipleRoutings);
+
+const setSingleRouting = (
+  req: Request, res: Response, next: any
+) => {
+  const newSource = req.body.source;
+  if (newSource == undefined) {
+    res.status(400);
+    res.send({ message: "No source specified" });
+    return;
+  }
+
+  const routing = { [req.params.stream]: newSource.toString() };
+  setRouting(routing, res, next);
+}
+
+server.put('/api/v1/routing/:stream', setSingleRouting);
 
 
 server.listen(3000, () => {
